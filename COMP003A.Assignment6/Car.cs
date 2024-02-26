3acd6f1fd65462079374ed77a23e8b3e0551e52c
BHIND-U <joshuadanielisidroreyes@gmail.com>
2024-02-26 08:51:41 -0800

    Add project files.

---
+++ COMP003A.Assignment6/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.Assignment6
+{
+    /// <summary>
+    /// Car extend the Vehicle class
+    /// </summary>
+    internal class Car : Vehicle
+    {
+        /***** constructors section *****/
+
+        /***** methods section *****/
+        /// <summary>
+        /// Overrides the base Vehicle's definintion for GetInfo()
+        /// </summary>
+
+        public override void GetInfo()
+        {
+            Console.WriteLine($"I'm a {nameof(Car)}");
+            Console.WriteLine("I don't want to disclose my information.");
+        }
+    }
+}
